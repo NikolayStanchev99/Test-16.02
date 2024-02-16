85895552d9fb3233da3d2fc1a9d0362fa988c417
NikolayStanchev99 <160015454+NikolayStanchev99@users.noreply.github.com>
2024-02-16 10:35:44 +0200

    Update Program.cs

--- test 16.02/Program.cs
+++ test 16.02/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Въведете възраст");
 int age = int.Parse(Console.ReadLine());
-if (age > 21)
+if (age > 24)
 {
     Console.WriteLine(Пълнолетен);
 }
